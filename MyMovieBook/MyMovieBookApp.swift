12e8f983bf12ccfe93720f131af9f1f9f1d599e7
bkulek20 <bkulek20@ku.edu.tr>
2024-01-03 17:27:53 +0300

    Initial Commit

---
+++ MyMovieBook/MyMovieBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMovieBookApp.swift
+//  MyMovieBook
+//
+//  Created by Büşra Sude Külek on 3.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyMovieBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
